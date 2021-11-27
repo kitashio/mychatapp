39893f6c157ae4a6733d26a07715957fcd9be3cf
kitashio <kitashio.pr@gmail.com>
2021-11-27 14:36:14 +0900

    firestore_test.dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,4 +1,5 @@
 // チャット画面用Widget
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'Login_page.dart';
@@ -14,7 +15,7 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('チャッ'),
+        title: Text('チャット'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.logout),
@@ -29,16 +30,54 @@ class ChatPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('ログイン成功：${user.email}'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text('ログイン情報：${user.email}'),
+          ),
+          Expanded(
+            // FutureBuilder
+            // 非同期処理の結果を元にWidgetを作れる
+            child: FutureBuilder<QuerySnapshot>(
+              // 投稿メッセージ一覧を取得（非同期処理）
+              // 投稿日時でソート
+              future: FirebaseFirestore.instance
+                  .collection('posts')
+                  .orderBy('date')
+                  .get(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data.docs;
+                  // 取得した投稿メッセージ一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(document['text']),
+                          subtitle: Text(document['email']),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+                // データが読込中の場合
+                return Center(
+                  child: Text('読込中...'),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () async {
-          // 投稿画面に遷移
-          await Navigator.of(context).push(
+          Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
-              return AddPostPage();
+              // 引数からユーザー情報を渡す
+              return AddPostPage(user);
             }),
           );
         },
